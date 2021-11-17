7f2c5aa2f5bfef01e1bb227eda3ac0b9710564d7
jduc <jduc@gaming.tech>
2021-11-17 12:43:43 +0100

    COMMIT 17/11 matin

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// NAVBAR RESPONSIVE
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+// SCRIPT CAROUSEL PROJETS ACCUEIL
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
